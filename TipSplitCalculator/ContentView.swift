b2c4f1a72e02defac735166ada1f05cb2536f227
akotetsu <yuanliqu@gmail.com>
2025-05-09 17:19:09 +0900

    Initial Commit

---
+++ TipSplitCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipSplitCalculator
+//
+//  Created by 原里駆 on 2025/05/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipSplitCalculator/TipSplitCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSplitCalculatorApp.swift
+//  TipSplitCalculator
+//
+//  Created by 原里駆 on 2025/05/09.
+//
+
+import SwiftUI
+
+@main
+struct TipSplitCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
